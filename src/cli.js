1c0975d26065f1777dd0a8bd13d9fba5f8e80312
Nikita L <starfall1011@gmail.com>
2021-06-02 18:45:37 +0300

    reworked exports

--- src/cli.js
+++ src/cli.js
@@ -1,10 +1,17 @@
 import readlineSync from 'readline-sync';
 
-const helloGetName = () => {
+export const helloGetName = () => {
   console.log('May I have your name?');
   const userName = readlineSync.question('Your answer: ');
   console.log(`Hello, ${userName}!`);
   return userName;
 };
 
-export { helloGetName };
+export const gcd = (a, b) => {
+  if (!b) {
+    return a;
+  }
+  return gcd(b, a % b);
+};
+
+export const getRandom = () => Math.floor(Math.random() * (100 - 1 + 1)) + 1; // random int 1 to 100
