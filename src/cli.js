2cbaee848f6065f7e9799ae21c6599c2821ac17a
Nikita L <starfall1011@gmail.com>
2021-06-03 13:19:30 +0300

    added new func for progression game

--- src/cli.js
+++ src/cli.js
@@ -15,3 +15,13 @@ export const gcd = (a, b) => {
 };
 
 export const getRandom = () => Math.floor(Math.random() * (100 - 1 + 1)) + 1; // random int 1 to 100
+
+export const getArray = () => {
+  const step = Math.round(1 + Math.random() * (6 - 1));
+  const arrLength = Math.round(7 + Math.random() * (12 - 7));
+  const arr = [step];
+  for (let i = 1; i < arrLength; i += 1) {
+    arr.push(arr[i - 1] + step);
+  }
+  return arr;
+};
