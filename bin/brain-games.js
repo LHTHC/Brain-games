e913f18ad4bf4ab3d0685189b60bae17d6199a83
Nikita L <starfall1011@gmail.com>
2021-06-07 18:11:01 +0300

    fixed brain-games.js

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,1 +1,7 @@
-console.log('Welcome to Brain Games!')
+#!/usr/bin/env node
+import readlineSync from 'readline-sync';
+
+console.log('Welcome to the Brain Games!');
+console.log('May I have your name?');
+const userName = readlineSync.question('Your answer: ');
+console.log(`Hello, ${userName}!`);
